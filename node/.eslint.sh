cd2807d212d83739ccdfe2710dcc5ba38f61cfff
lazidog <nvtien247@gmail.com>
2023-09-19 11:14:13 +0700

    .

--- node/.eslint.sh
+++ node/.eslint.sh
@@ -3,3 +3,7 @@ sudo npx install-peerdeps -g eslint-config-airbnb
 ln -s /opt/homebrew/lib/node_modules ~/
 
 ln -sf ~/.dotfiles/node/.eslintrc.js ~/.eslintrc.js
+
+npx install-peerdeps -g eslint-config-airbnb
+
+ln -s /opt/homebrew/lib/node_modules ~/
